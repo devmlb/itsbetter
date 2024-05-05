480517cf0c54a3f59973e1fa3afdd12c4be09d52
Mlb <97614168+devmlb@users.noreply.github.com>
2024-05-05 18:57:18 +0200

    New look for navbar

--- mods/navbar/navbar.js
+++ mods/navbar/navbar.js
@@ -1,13 +1,41 @@
 (function() {
+    const iconsBarUl = document.querySelector(".l-personal-menu-items.h-hlist");
     try {
-        const workspacesBtn = document.querySelectorAll('[data-action="GetCourses"]')[0];
-        const workspacesMenu = document.querySelectorAll('[data-dropdown="courses"]')[0];
+        const workspacesLi = document.querySelector('.l-tab-menu-text.l-menu-course.js-menuitem--dropdown').cloneNode(true);
+        workspacesLi.setAttribute("class", "l-personal-badge-menu-item h-position-r");
+        workspacesLi.removeChild(workspacesLi.querySelector("li>div"));
+        workspacesLi.removeChild(workspacesLi.querySelector("li>script"));
+
+        const workspacesIcon = workspacesLi.querySelector("li>button>img");
+        workspacesIcon.src = chrome.runtime.getURL("mods/material-icons-round-svg/school_black_24dp.svg");
+        workspacesIcon.setAttribute("class", "h-va-middle l-personal-menu-icon-notifications l-menu-icons");
+        workspacesIcon.setAttribute("style", "height:24px;width:24px;");
+
+        const workspacesBtn = workspacesLi.querySelector('li>button');
+        workspacesBtn.setAttribute("class", "h-position-r h-pdl5 h-pdr5 l-personal-menu-items-link");
+        workspacesBtn.setAttribute("id", "Workspaces");
+        workspacesBtn.removeAttribute("data-action")
+
+        const workspacesMenu = document.querySelector('[data-dropdown="courses"]');
+        workspacesBtn.addEventListener("click", function() {
+            console.log(workspacesMenu.style.display);
+            if (workspacesMenu.style.display == 'none') {
+                document.querySelector('.l-menu-course>button').click();
+            } else {
+                document.querySelector('.l-menu-course>button').click();
+            }
+        });
+
+        iconsBarUl.prepend(workspacesLi);
+
         let worspacesMenuObserver = new MutationObserver(function (mutations) {
             mutations.forEach(function (mutationRecord) {
                 if (workspacesMenu.style.display == 'block') {
                     workspacesBtn.classList.add("navbar-menu-opened");
+                    workspacesIcon.classList.add("navbar-menu-opened");
                 } else {
                     workspacesBtn.classList.remove("navbar-menu-opened");
+                    workspacesIcon.classList.remove("navbar-menu-opened");
                 }
             });
         });
@@ -20,14 +48,40 @@
     }
 
     try {
-        const communitiesBtn = document.querySelectorAll('[data-action="GetGroups"]')[0];
-        const communitiesMenu = document.querySelectorAll('[data-dropdown="data-projects-dropdown"]')[0];
+        const communitiesLi = document.querySelector('.l-tab-menu-text.l-menu-project.js-menuitem--dropdown').cloneNode(true);
+        communitiesLi.setAttribute("class", "l-personal-badge-menu-item h-position-r");
+        communitiesLi.removeChild(communitiesLi.querySelector("li>div"));
+
+        const communitiesIcon = communitiesLi.querySelector("li>button>img");
+        communitiesIcon.src = chrome.runtime.getURL("mods/material-icons-round-svg/domain_black_24dp.svg");
+        communitiesIcon.setAttribute("class", "h-va-middle l-personal-menu-icon-notifications l-menu-icons");
+        communitiesIcon.setAttribute("style", "height:24px;width:24px;");
+
+        const communitiesBtn = communitiesLi.querySelector('li>button');
+        communitiesBtn.setAttribute("class", "h-position-r h-pdl5 h-pdr5 l-personal-menu-items-link");
+        communitiesBtn.setAttribute("id", "Communities");
+        communitiesBtn.removeAttribute("data-action")
+
+        const communitiesMenu = document.querySelector('[data-dropdown="data-projects-dropdown"]');
+        communitiesBtn.addEventListener("click", function() {
+            console.log(communitiesMenu.style.display);
+            if (communitiesMenu.style.display == 'none') {
+                document.querySelector('.l-menu-project>button').click();
+            } else {
+                document.querySelector('.l-menu-project>button').click();
+            }
+        });
+
+        iconsBarUl.prepend(communitiesLi);
+
         let communitiesMenuObserver = new MutationObserver(function (mutations) {
             mutations.forEach(function (mutationRecord) {
                 if (communitiesMenu.style.display == 'block') {
                     communitiesBtn.classList.add("navbar-menu-opened");
+                    communitiesIcon.classList.add("navbar-menu-opened");
                 } else {
                     communitiesBtn.classList.remove("navbar-menu-opened");
+                    communitiesIcon.classList.remove("navbar-menu-opened");
                 }
             });
         });
@@ -37,6 +91,29 @@
         });
     } catch (e) {
         console.log("Error when applying style to the navbar communities button: "+e+" You can ignore this error as long as it does not affect your use of Itslearning.");
+    }
+
+    try {
+        const notifsBtn = document.querySelector('#Notification');
+        const notifsMenu = document.querySelector('#notifications-overlay-wrapper');
+        const notifsIcon = notifsBtn.querySelector('button>img');
+        let notifsMenuObserver = new MutationObserver(function (mutations) {
+            mutations.forEach(function (mutationRecord) {
+                if (notifsMenu.style.display == 'block') {
+                    notifsBtn.classList.add("navbar-menu-opened");
+                    notifsIcon.classList.add("navbar-menu-opened");
+                } else {
+                    notifsBtn.classList.remove("navbar-menu-opened");
+                    notifsIcon.classList.remove("navbar-menu-opened");
+                }
+            });
+        });
+        notifsMenuObserver.observe(notifsMenu, {
+            attributes: true,
+            attributeFilter: ['style']
+        });
+    } catch (e) {
+        console.log("Error when applying style to the navbar notifications button: "+e+" You can ignore this error as long as it does not affect your use of Itslearning.");
     }
 
     try {
