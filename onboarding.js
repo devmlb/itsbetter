97e6a79a449f1288fead70558646d384d59fa379
Mlb <97614168+devmlb@users.noreply.github.com>
2024-05-29 22:20:48 +0200

    Preparing onboarding to request permissions on Firefox

---
+++ onboarding.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    mdui.setColorScheme('#f47920');
+    const btn = document.getElementById("permission-btn");
+    const snackbar = document.getElementById("snackbar");
+    btn.addEventListener("click", function() {
+        browser.permissions.request({ origins: ['https://*.itslearning.com/*'] });
+        snackbar.open = true;
+    });
+});
