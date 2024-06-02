dfd038448e9215fca6b60400751d035800704210
Mlb <97614168+devmlb@users.noreply.github.com>
2024-06-02 11:25:35 +0200

    Releasing onboarding page

--- internal/onboarding.js
+++ internal/onboarding.js
@@ -1,10 +1,9 @@
-
 document.addEventListener("DOMContentLoaded", function () {
     mdui.setColorScheme('#f47920');
     const btn = document.getElementById("permission-btn");
     const snackbar = document.getElementById("snackbar");
     btn.addEventListener("click", function() {
-        browser.permissions.request({ origins: ['https://*.itslearning.com/*'] });
+        chrome.permissions.request({ origins: ['https://*.itslearning.com/*'] });
         snackbar.open = true;
     });
 });
